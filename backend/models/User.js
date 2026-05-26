bc8ff08f031f6deae9b866ce3ed30043be1aba12
manju2810 <manju010902@gmail.com>
2026-05-26 16:30:47 +0530

    created user model

---
+++ backend/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true,
+      },
+      role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+      },
+      is_active: {
+        type: Boolean,
+        default: true,
+      },
+      created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: null,
+      },
+    },
+    { timestamps: true }
+  );userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  });
+  userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  });
+  
+  // Password compare karne ka method
+  userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  };
+  
+  module.exports = mongoose.model("User", userSchema);
