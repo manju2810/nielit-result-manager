19061d6ae3cd7c878e870c453ff79f42b9548f98
manju2810 <manju010902@gmail.com>
2026-07-31 10:20:35 +0530

    wire up delete/rollback exam cycle route

--- backend/routes/resultRoutes.js
+++ backend/routes/resultRoutes.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { listStudents, listBatches, getStudent, exportStudents, listCycles } = require("../controllers/resultController");
-const { protect } = require("../middleware/auth");
 const { deleteExamCycle } = require("../controllers/uploadController");
-const { authorize } = require("../middleware/role"); // or whatever your admin check is called
-
-router.delete("/cycles/:id", protect, authorize("admin"), deleteExamCycle);
+const { protect } = require("../middleware/auth");
+const { adminOnly } = require("../middleware/role");
 
 router.get("/students", protect, listStudents);
 router.get("/batches", protect, listBatches);
 router.get("/export", protect, exportStudents);
 router.get("/students/:regn_no", protect, getStudent);
 router.get("/cycles", protect, listCycles);
+router.delete("/cycles/:id", protect, adminOnly, deleteExamCycle);
 
 module.exports = router;
